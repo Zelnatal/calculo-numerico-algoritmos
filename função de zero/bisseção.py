661455d3de9e42210b43378b816a058e904cee40
Zelnatal <elnatan.honorato@gmail.com>
2023-12-08 18:37:21 -0300

    Método da bisseção em python

---
+++ função de zero/bisseção.py
@@ -0,0 +1,43 @@
+import math
+def bisseção(f,p,a,b):
+    epsilon = 10**(-p)
+    fa = f(a)
+    fb = f(b)
+    
+    if fa*fb > 0:
+        return ValueError()
+    
+    while (abs(a-b) >= epsilon and abs(fa) >= epsilon and abs(fb) > epsilon):
+        x = (a+b)/2
+        fx = f(x)
+        if fa*fx < 0:
+            b = x
+            fb = fx
+            continue
+        if fb*fx < 0:
+            a = x
+            fa = fx
+            continue
+        return RuntimeError()
+    
+    if abs(a-b) < epsilon or abs(fa) < epsilon:
+        return a
+    if abs(fb) < epsilon:
+        return b
+    return RuntimeError()
+
+if __name__ == '__main__':
+    f = lambda x: math.sin(x)*x**x
+    p = 15
+    a = 6
+    b = 7
+    resultado = bisseção(f,p,a,b)
+    if isinstance(resultado,BaseException):
+        match resultado:
+            case ValueError():
+                print("Intervalo invalido")
+            case _:
+                print("Deu error")
+    else:
+        print(f'A raiz encontrada é {resultado}')
+        print(f'F({resultado}) é {f(resultado)}')
