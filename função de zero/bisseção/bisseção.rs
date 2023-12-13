577abd1f7158923caea096c8d4dee44dbb1edffc
Zelnatal <elnatan.honorato@gmail.com>
2023-12-12 22:01:31 -0300

    Melhorando o método da bisseção em rust

--- função de zero/bisseção/bisseção.rs
+++ função de zero/bisseção/bisseção.rs
@@ -1,11 +1,25 @@
+use std::collections::VecDeque;
+
 type Função = fn(f64) -> f64;
 
 enum Erro {
     IntervaloInvalido,
+    IteraçõesMáxima(u32),
     Desconhecido(String),
 }
 
-fn bisseção(f: &Função, epsilon: f64, mut a: f64, mut b: f64) -> Result<f64, Erro> {
+fn iter_máximo(a: f64, b: f64, epsilon: f64) -> u32 {
+    return ((b - a).abs().log2() - epsilon.log2()).ceil() as u32;
+}
+
+fn bisseção(
+    f: &Função,
+    epsilon: f64,
+    mut a: f64,
+    mut b: f64,
+    mut k_atual: u32,
+    k_máximo: u32,
+) -> Result<f64, Erro> {
     let mut fa = f(a);
     let mut fb = f(b);
 
@@ -13,7 +27,12 @@ fn bisseção(f: &Função, epsilon: f64, mut a: f64, mut b: f64) -> Result<f64,
         return Err(Erro::IntervaloInvalido);
     }
 
-    while (a - b).abs() >= epsilon && fa.abs() >= epsilon && fb.abs() >= epsilon {
+    while k_atual <= k_máximo
+        && (a - b).abs() >= epsilon
+        && fa.abs() >= epsilon
+        && fb.abs() >= epsilon
+    {
+        k_atual += 1;
         let x = (a + b) / 2.0;
         let fx = f(x);
 
@@ -32,6 +51,10 @@ fn bisseção(f: &Função, epsilon: f64, mut a: f64, mut b: f64) -> Result<f64,
         return Err(Erro::Desconhecido("Erro dentro do loop".to_string()));
     }
 
+    if k_atual > k_máximo {
+        return Err(Erro::IteraçõesMáxima(k_máximo));
+    }
+
     if (a - b).abs() < epsilon || fa.abs() < epsilon {
         return Ok(a);
     }
@@ -45,18 +68,69 @@ fn bisseção(f: &Função, epsilon: f64, mut a: f64, mut b: f64) -> Result<f64,
     ))
 }
 
-fn main() {
-    let f: Função = |x| x.sin() * x.powf(x);
-    let epsilon = 10.0_f64.powi(-15);
-    let a = 6.0;
-    let b = 7.0;
+fn encontrar_intervalo(
+    f: &Função,
+    a: f64,
+    b: f64,
+    k_máximo: u32,
+) -> Option<(f64, f64, u32)> {
+    let fa = f(a);
+    let fb = f(b);
+    let k_atual: u32 = 0;
 
-    match bisseção(&f, epsilon, a, b) {
-        Ok(resultado) => {
-            println!("A raiz encontrada é {}", resultado);
-            println!("F({}) é {}", resultado, f(resultado))
-        }
-        Err(Erro::IntervaloInvalido) => println!("O intervalo precisa cruza o eixo x"),
-        Err(Erro::Desconhecido(mensagem)) => println!("Deu Erro: {}", mensagem),
+    if fa * fb < 0.0 {
+        return Some((a, b, k_atual));
     }
+
+    let mut fila: VecDeque<(f64,f64,u32)> = VecDeque::new();
+    fila.push_front((a,b,k_atual));
+
+    while fila.len() > 0 {
+        let (aa,ba,ka) =  fila.pop_back().unwrap();
+        if ka > k_máximo{
+            continue;
+        }
+        let x = (aa+ba)/2.0;
+        let fx = f(x); 
+
+        if fx*f(aa) < 0.0{
+            return Some((aa,x,ka+1));
+        }
+
+        if fx*f(ba) < 0.0{
+            return Some((ba,x,ka+1));
+        }
+
+        fila.push_front((aa,x,ka+1));
+        fila.push_front((ba,x,ka+1));
+    }
+
+    None
+}
+
+fn main() {
+    let f: Função = |x| 40.0 - 32.7 * x + 108.88 * (1.0 - (-0.3 * x).exp());
+    let epsilon = 10.0_f64.powi(-3);
+    let a = -2.0;
+    let b = 3.5;
+
+    let k_máximo = iter_máximo(a, b, epsilon);
+
+    if let Some((a_atual,b_atual,k_atual)) = encontrar_intervalo(&f, a, b, k_máximo){
+        match bisseção(&f, epsilon, a_atual, b_atual,k_atual,k_máximo) {
+            Ok(resultado) => {
+                let corta = (resultado / epsilon).trunc() * epsilon;
+                println!("A raiz encontrada é {}", corta);
+                println!("F({}) é {}", corta, f(corta))
+            }
+            Err(Erro::IntervaloInvalido) => println!("O intervalo precisa cruza o eixo x"),
+            Err(Erro::IteraçõesMáxima(máximo)) => {
+                println!("Ultrapassou o limite de iterações maxima de {}", máximo)
+            }
+            Err(Erro::Desconhecido(mensagem)) => println!("Deu Erro: {}", mensagem),
+        }
+    } else{
+        println!("Melhore o intervalo");
+    }
+
 }
