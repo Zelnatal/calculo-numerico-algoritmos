e7b6d9b525a6ffddc3877e22239c5b2d950c8382
= <=>
2024-02-01 12:37:16 -0300

    Corrigindo bug da matriz superior

--- matriz triangular superior/matriz triangular superior.rs
+++ matriz triangular superior/matriz triangular superior.rs
@@ -1,7 +1,7 @@
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 struct Matriz {
     matriz: Vec<Vec<f64>>,
-    b: Vec<f64>
+    b: Vec<f64>,
 }
 
 #[derive(Debug)]
@@ -12,15 +12,11 @@ enum Error {
 }
 
 impl Matriz {
-
     fn new(matriz: Vec<Vec<f64>>, b: Vec<f64>) -> Self {
-        if matriz.len() != 0 && matriz[0].len() != b.len(){
+        if matriz.len() != 0 && matriz[0].len() != b.len() {
             panic!("A coluna da matriz tem que ter o mesmo tamanho de b")
         }
-        Self {
-            matriz,
-            b
-        }
+        Self { matriz, b }
     }
 
     fn m(&self) -> usize {
@@ -28,7 +24,7 @@ impl Matriz {
     }
 
     fn n(&self) -> usize {
-        if self.m() == 0{
+        if self.m() == 0 {
             0
         } else {
             self.matriz[0].len()
@@ -51,34 +47,38 @@ impl Matriz {
         let matriz = &mut retorno.matriz;
         let b = &mut retorno.b;
         let mut k = 0;
-        'k: while k < matriz.len()-1{
+        'k: while k < matriz.len() - 1 {
             if matriz[k][k] == 0.0 {
-                for i in k+1 .. k {
+                for i in k + 1..matriz.len() {
                     if matriz[i][k] != 0.0 {
                         matriz.swap(i, k);
+                        b.swap(i, k);
                         continue 'k;
                     }
                 }
                 return Err(Error::NãoTemTriangular);
             }
-            for i in k+1..matriz.len() {
-                let mul = matriz[i][k]/matriz[k][k];
+            for i in k + 1..matriz.len() {
+                let mul = matriz[i][k] / matriz[k][k];
                 matriz[i][k] = 0.0;
-                for j in k+1 .. matriz.len() {
-                    matriz[i][j] -= mul*matriz[k][j];
+                for j in k + 1..matriz.len() {
+                    matriz[i][j] -= mul * matriz[k][j];
                 }
-                b[i] -= mul*b[k];
+                b[i] -= mul * b[k];
             }
             k += 1
-
         }
         Ok(retorno)
-    } 
+    }
 }
 
 fn main() {
-    let m = vec![vec![3.0,2.0,4.0],vec![1.0,1.0,2.0],vec![4.0,3.0,-2.0]];
-    let b = vec![1.0,2.0,3.0];
+    let m = vec![
+        vec![3.0, 2.0, 4.0],
+        vec![1.0, 1.0, 2.0],
+        vec![4.0, 3.0, -2.0],
+    ];
+    let b = vec![1.0, 2.0, 3.0];
     let matriz = Matriz::new(m, b);
-    println!("{:#?}",matriz.converter_em_triangular_superior())
+    println!("{:#?}", matriz.converter_em_triangular_superior())
 }
