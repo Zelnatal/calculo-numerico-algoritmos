ccbef19c85d756ca4dabafa54575c4432639db1f
= <=>
2024-02-01 17:28:54 -0300

    Melhorando a eliminação de gauss

--- sistemas lineares/eliminação de gauss/eliminação de gauss.rs
+++ sistemas lineares/eliminação de gauss/eliminação de gauss.rs
@@ -1,71 +1,53 @@
 #[derive(Debug, Clone)]
-struct Matriz {
-    matriz: Vec<Vec<f64>>,
+struct SistemaEquação {
+    a: Vec<Vec<f64>>,
     b: Vec<f64>,
 }
 
 #[derive(Debug)]
 enum Error {
     NãoQuadrada,
-    NãoTemTriangular,
-    ColunaLenDiferenteB,
-    DivisãoZero,
+    AVazio,
+    LenColunaADiferenteB,
+    NãoPossível
 }
 
-impl Matriz {
-    fn new(matriz: Vec<Vec<f64>>, b: Vec<f64>) -> Self {
-        if matriz.len() != 0 && matriz[0].len() != b.len() {
-            panic!("A coluna da matriz tem que ter o mesmo tamanho de b")
+impl SistemaEquação {
+    fn new(a: Vec<Vec<f64>>, b: Vec<f64>) -> Self {
+        Self { a, b }
+    }
+
+    fn preparar_sistema_triangular_superior(&self) -> Result<Self, Error> {
+        if self.a.len() == 0 {
+            return Err(Error::AVazio);
         }
-        Self { matriz, b }
-    }
-
-    fn m(&self) -> usize {
-        self.matriz.len()
-    }
-
-    fn n(&self) -> usize {
-        if self.m() == 0 {
-            0
-        } else {
-            self.matriz[0].len()
-        }
-    }
-
-    fn b_len(&self) -> usize {
-        self.b.len()
-    }
-
-    fn converter_em_triangular_superior(&self) -> Result<Self, Error> {
-        if self.m() != self.n() {
+        if self.a.len() != self.a[0].len() {
             return Err(Error::NãoQuadrada);
         }
-        if self.n() != self.b_len() {
-            return Err(Error::ColunaLenDiferenteB);
+        if self.a[0].len() != self.b.len() {
+            return Err(Error::LenColunaADiferenteB);
         }
 
         let mut retorno = self.clone();
-        let matriz = &mut retorno.matriz;
-        let b = &mut retorno.b;
         let mut k = 0;
-        'k: while k < matriz.len() - 1 {
-            if matriz[k][k] == 0.0 {
-                for i in k + 1..matriz.len() {
-                    if matriz[i][k] != 0.0 {
-                        matriz.swap(i, k);
-                        b.swap(i, k);
+        'k: while k < retorno.a.len() {
+            if retorno.a[k][k] == 0.0 {
+                for i in k + 1..retorno.a.len() {
+                    if retorno.a[i][k] != 0.0 {
+                        retorno.a.swap(i, k);
+                        retorno.b.swap(i, k);
                         continue 'k;
                     }
                 }
-                return Err(Error::NãoTemTriangular);
+                return Err(Error::NãoPossível);
             }
-            for i in k + 1..matriz.len() {
-                let mul = matriz[i][k] / matriz[k][k];
-                matriz[i][k] = 0.0;
-                for j in k + 1..matriz.len() {
-                    matriz[i][j] -= mul * matriz[k][j];
+            for i in k + 1..retorno.a.len() {
+                let mul = retorno.a[i][k] / retorno.a[k][k];
+                retorno.a[i][k] = 0.0;
+                for j in k + 1..retorno.a.len() {
+                    retorno.a[i][j] -= mul * retorno.a[k][j];
                 }
-                b[i] -= mul * b[k];
+                retorno.b[i] -= mul * retorno.b[k];
             }
             k += 1
         }
@@ -73,48 +55,39 @@ impl Matriz {
     }
 }
 
-fn gauss(matriz: &Matriz) -> Result<Vec<f64>, Error> {
-    let triangular = matriz.converter_em_triangular_superior()?;
-    if triangular.b_len() == 0 {
-        return Ok(vec![]);
-    }
-    let b = &triangular.b;
-    let m = &triangular.matriz;
-
-    let mut xs = vec![0.0; matriz.b_len()];
-    for i in (0..b.len()).rev() {
-        if m[i][i] == 0.0 {
-            return Err(Error::DivisãoZero);
-        }
+fn gauss(sistema: &SistemaEquação) -> Result<Vec<f64>, Error> {
+    let sistema = sistema.preparar_sistema_triangular_superior()?;
+    let mut xs = vec![0.0; sistema.b.len()];
+    for i in (0..sistema.b.len()).rev() {
         let mut s = 0.0;
-        for j in i + 1..b.len() {
-            s += m[i][j] * xs[j]
+        for j in i + 1..sistema.b.len() {
+            s += sistema.a[i][j] * xs[j]
         }
-        xs[i] = (b[i] - s) / m[i][i];
+        xs[i] = (sistema.b[i] - s) / sistema.a[i][i];
     }
     Ok(xs)
 }
 
 fn main() {
-    let m = vec![
-        vec![0.0, 2.0, 4.0],
-        vec![1.0, 2.0, 2.0],
-        vec![4.0, 3.0, 0.0],
+    let a = vec![
+        vec![3.0, 2.0, 4.0],
+        vec![1.0, 1.0, 2.0],
+        vec![4.0, 3.0, -2.0], 
     ];
     let b = vec![1.0, 2.0, 3.0];
-    let matriz = Matriz::new(m, b);
-    match gauss(&matriz) {
+    let sistema = SistemaEquação::new(a, b);
+    match gauss(&sistema) {
         Ok(xs) => {
             println!("Os xs encontrados são :");
             for x in xs {
                 println!("{}", x)
             }
+        },
+        Err(Error::AVazio) => println!("O matriz A não pode está vazio"),
+        Err(Error::NãoQuadrada) => println!("A matriz A não é quadrada"),
+        Err(Error::LenColunaADiferenteB) => {
+            println!("O tamanho da coluna da matriz A é diferente do tamanho de Vetor B")
         }
-        Err(Error::NãoQuadrada) => println!("A matriz não é quadrada"),
-        Err(Error::ColunaLenDiferenteB) => {
-            println!("O tamanho da coluna da matriz é diferente do tamanho de b")
-        }
-        Err(Error::NãoTemTriangular) => println!("Não foi possível converte em triangular"),
-        Err(Error::DivisãoZero) => println!("Teve uma divisão por zero"),
+        Err(Error::NãoPossível) => println!("Não foi possível resolver pelo matriz triangular superior"),
     }
 }
