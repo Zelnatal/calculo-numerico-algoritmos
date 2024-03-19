bedd57d303eb847231538cea0866cc5c9476e913
Zelnatal <elnatan.honorato@gmail.com>
2024-03-19 11:22:40 -0300

    adicionando riemann

---
+++ integral/riemann/riemann.rs
@@ -0,0 +1,17 @@
+fn riemann(a: f64, b: f64, n: usize, f: fn(f64) -> f64) -> f64 {
+    let dx = (b - a) / (n as f64);
+    (1..=n)
+        .map(|i| {
+            print!("{}/{}\r", i, n);
+            dx * f(a + dx * (i as f64))
+        })
+        .sum::<f64>()
+}
+
+fn main() {
+    let f = |x: f64| x;
+    let a = 0.0;
+    let b = 3.0;
+    let n = 1000;
+    println!("A integral é {}", riemann(a, b, n, f));
+}
