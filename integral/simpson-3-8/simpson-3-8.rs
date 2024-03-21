9721bf38a3f2f37915ed5037a0040b2abd9a348c
Zelnatal <elnatan.honorato@gmail.com>
2024-03-21 18:47:05 -0300

    adicionando o simpson 3/8

---
+++ integral/simpson-3-8/simpson-3-8.rs
@@ -0,0 +1,28 @@
+fn simpson(a: f64, b: f64, n: usize, f: fn(f64) -> f64) -> Result<f64, &'static str> {
+    if !(n % 3 == 0 && n >= 3) {
+        return Err("O n tem que ser par maior ou igual a 3");
+    }
+    let h = (b - a) / (n as f64);
+    Ok((0..=n)
+        .map(|i| {
+            let xi = a + h * (i as f64);
+            match i {
+                0 | _ if i == n => f(xi),
+                _ if i % 3 == 1 || i%3 == 2 => 3.0 * f(xi),
+                _ if i % 3 == 0 => 2.0 * f(xi),
+                _ => unreachable!(),
+            }
+        })
+        .sum::<f64>() * (3.0*h/8.0))
+}
+
+fn main() {
+    let f = |x: f64| x;
+    let a = 0.0;
+    let b = 3.0;
+    let n = 1002;
+    match simpson(a, b, n, f) {
+        Ok(r) => println!("A integral é {}",r),
+        Err(s) => eprintln!("Erro: {}",s)
+    }
+}
