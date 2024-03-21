e604be178da0a3f43537a08092ba05ec3436aaff
Zelnatal <elnatan.honorato@gmail.com>
2024-03-21 18:35:04 -0300

    adicionando o metodo simpson 1/3

---
+++ integral/simpson-1-3/simpson-1-3.rs
@@ -0,0 +1,28 @@
+fn simpson(a: f64, b: f64, n: usize, f: fn(f64) -> f64) -> Result<f64, &'static str> {
+    if !(n % 2 == 0 && n >= 2) {
+        return Err("O n tem que ser par maior ou igual a 2");
+    }
+    let h = (b - a) / (n as f64);
+    Ok((0..=n)
+        .map(|i| {
+            let xi = a + h * (i as f64);
+            match i {
+                0 | _ if i == n => f(xi),
+                _ if i % 2 == 1 => 4.0 * f(xi),
+                _ if i % 2 == 0 => 2.0 * f(xi),
+                _ => unreachable!(),
+            }
+        })
+        .sum::<f64>() * (h/3.0))
+}
+
+fn main() {
+    let f = |x: f64| x;
+    let a = 0.0;
+    let b = 3.0;
+    let n = 1000;
+    match simpson(a, b, n, f) {
+        Ok(r) => println!("A integral é {}",r),
+        Err(s) => eprintln!("Erro: {}",s)
+    }
+}
